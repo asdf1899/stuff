121a223b6c394eec9adc21896cbdbf3f9d9ed390
asdf1899 <araid.anas99@gmail.com>
2020-03-10 12:19:41 +0100

    Add first serious article

--- src/components/ArticleContent.vue
+++ src/components/ArticleContent.vue
@@ -27,16 +27,10 @@
             }
         },
         data: function(){
-            var body = document.body,
-                html = document.documentElement;
-
-            var height = Math.max( body.scrollHeight, body.offsetHeight, 
-                            html.clientHeight, html.scrollHeight, html.offsetHeight );
-            height = height /2;
             let jsonArticle = jsonArticles.find(x => x.id === this.id);
             
             const markdownFile = require('../statics/markdowns/'+jsonArticle.src).default;
-            return ({ markdownFile, jsonArticle, height });
+            return ({ markdownFile, jsonArticle });
         }
     }
 </script>
