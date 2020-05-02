5f6aec9c693f810e2946c6e7550595b270c7f056
asdf1899 <araid.anas99@gmail.com>
2020-05-02 22:47:40 +0200

    Add analytics

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router.js'
+import VueAnalytics from 'vue-analytics';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(VueAnalytics, {
+  id: process.env.VUE_APP_ANALYTICS,
+  router
+});
+
 
 new Vue({ el: '#app', router, render: h => h(App) })
